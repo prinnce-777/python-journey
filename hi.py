6fe569143df0299717fb0aee4a70061b325d3236
prinnce-777 <princeenosh80@gmail.com>
2026-08-11 21:26:58 +0530

    added modif code

--- hi.py
+++ hi.py
@@ -1,1 +1,7 @@
-print("hello")
+name = input("enter-")
+vowels =["a","e","i","o","u"]
+count = 0
+for i in range(len(name)):
+    if name[i] in vowels:
+        count += 1
+print(f"there are {count} vowels in  word {name}")
